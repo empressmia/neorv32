f6f549db1de6eecb4557004646cd434b900c9d1a
EmpressMia <191341940+empressmia@users.noreply.github.com>
2025-01-01 21:18:28 +0100

    added functions and enum type to firq-crossbar header.

--- sw/lib/include/neorv32_firq_crossbar.h
+++ sw/lib/include/neorv32_firq_crossbar.h
@@ -21,10 +21,29 @@
 #include <stdint.h>
 
 typedef volatile struct __attribute__((packed,aligned(4))) {
-  uint32_t ch_en_mask;      /** < offset o: output channel enabled; i.e. forwards irq to cpu */
-  uint32_t ch_wrpr_mask[2]; /** < offset 4: */
-  uint32_t ch_assign[3];    /** < offset 8: */
+  uint32_t CH_EN_MASK;      /** < offset o: output channel enabled; i.e. forwards irq to cpu */
+  uint32_t CH_WRPR_MASK[2]; /** < offset 4: */
+  uint32_t CH_ASSIGN[3];    /** < offset 8: */
 } neorv32_firq_crossbar_t;
+
+enum NEORV32_FIRQ_CB_WRPR_CH_enum {
+ WRPR_CH_0  =  0,
+ WRPR_CH_1  =  2,
+ WRPR_CH_2  =  4,
+ WRPR_CH_3  =  6,
+ WRPR_CH_4  =  8,
+ WRPR_CH_5  = 10,
+ WRPR_CH_6  = 12,
+ WRPR_CH_7  = 14,
+ WRPR_CH_8  = 16,
+ WRPR_CH_9  = 18,
+ WRPR_CH_10 = 20,
+ WRPR_CH_11 = 22,
+ WRPR_CH_12 = 24,
+ WRPR_CH_13 = 26,
+ WRPR_CH_14 = 28,
+ WRPR_CH_15 = 30
+} neorv32_firq_cb_wrpr_ch_t;
 
 enum NEORV32_FIRQ_CB_WRPR_enum {
   CH_PROT_LEVEL_0 = 0,
@@ -37,8 +56,9 @@ enum NEORV32_FIRQ_CB_WRPR_enum {
 #define NEORV32_FIRQ_CB ((neorv32_firq_crossbar_t*) (NEORV32_FIRQ_CB_BASE))
 
 int neorv32_firq_cb_available(void);
-int neorv32_firq_cb_ch_enable(const uint16_t ch);
-int neorv32_firq_cb_ch_wrpr(const uint16_t ch, const enum NEORV32_FIRQ_CB_WRPR_enum level);
+int neorv32_firq_cb_ch_enable(const uint16_t ch_num);
+int neorv32_firq_cb_ch_disable(const uint16_t ch_num);
+int neorv32_firq_cb_ch_wrpr(const enum NEORV32_FIRQ_CB_WRPR_CH_enum ch, const enum NEORV32_FIRQ_CB_WRPR_enum level);
 int neorv32_firq_cb_ch_assign(const uint16_t ch, const uint8_t input_ch_num);
 enum NEORV32_FIRQ_CB_WRPR_enum neorv32_firq_cb_get_wrpr(const uint16_t ch);
 
